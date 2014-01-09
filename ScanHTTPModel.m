71c563ac9f5184ed1ac650d5ff381ac0ea40406a
Crae Sosa <csosa246@gmail.com>
2014-01-09 11:30:42 -0500

    commiting

--- ScanHTTPModel.m
+++ ScanHTTPModel.m
@@ -15,16 +15,11 @@
 
 -(void) serverConfirmation:(NSString*)pid bid:(NSString*) bid{
 //    [self alert:YES];
-    
     self.responseData = [NSMutableData data];
     NSMutableURLRequest *request=[NSMutableURLRequest requestWithURL:[NSURL URLWithString:[@"https://bluecanaryalpha.herokuapp.com/mobile" stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:60.0];
     [request setHTTPMethod:@"POST"];
     [request setValue:@"application/json" forHTTPHeaderField:@"content-type"];
-    NSMutableDictionary *tmp;
-    
-    tmp = [[NSMutableDictionary alloc] initWithObjectsAndKeys: pid, @"id", bid, @"bid", nil];
-    
-    //    NSLog(@"%@",tmp);
+    NSMutableDictionary *tmp = [[NSMutableDictionary alloc] initWithObjectsAndKeys: pid, @"id", bid, @"bid", nil];
     NSError *error;
     NSData *postdata = [NSJSONSerialization dataWithJSONObject:tmp options:0 error:&error];
     [request setHTTPBody:postdata];
@@ -48,9 +43,8 @@
     NSError *myError = nil;
     NSDictionary *data = [NSJSONSerialization JSONObjectWithData:self.responseData options:NSJSONReadingMutableLeaves error:&myError];
     NSString *response = [[NSString alloc] initWithData:responseData encoding:NSASCIIStringEncoding];
-    NSString *parsed = [response stringByReplacingOccurrencesOfString:@"\\" withString:@""];
-    NSLog(parsed);
-    
+//    NSString *parsed = [response stringByReplacingOccurrencesOfString:@"\\" withString:@""];
+//    NSLog(parsed);
 //    [[self delegate] scanHTTPconnectionDidFinishLoading:data];
 }
 
