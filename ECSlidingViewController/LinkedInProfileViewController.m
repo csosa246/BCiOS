83b87a3ddba51bb8d2d19d32b6ce237fa821c76f
Crae Sosa <csosa246@gmail.com>
2014-01-17 11:59:14 -0500

    edited linkedinviewcontroller

--- ECSlidingViewController/LinkedInProfileViewController.m
+++ ECSlidingViewController/LinkedInProfileViewController.m
@@ -30,19 +30,6 @@
     NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
     [webView loadRequest:requestObj];
     
-//    //Loading dialog
-//    alert = [[UIAlertView alloc] initWithTitle:@"Loading..." message:nil delegate:self cancelButtonTitle:nil otherButtonTitles: nil];
-//    [alert show];
-//    
-//    self.responseData = [NSMutableData data];
-//    NSMutableURLRequest *request=[NSMutableURLRequest requestWithURL:[NSURL URLWithString:[@"https://bluecanaryconnect.appspot.com/apphandle" stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]] cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:60.0];
-//    [request setHTTPMethod:@"POST"];
-//    [request setValue:@"ed" forHTTPHeaderField:@"NAME"];
-//    [request setValue:@"ed" forHTTPHeaderField:@"PASSWORD"];
-//    [request setValue:@"LINKEDIN" forHTTPHeaderField:@"COMMAND"];
-//    [request setValue:linkedinURL forHTTPHeaderField:@"UUID"];
-//    //[request setValue:@"A2F01946-7461-BB1B-1C9E-CD2749EC2977" forHTTPHeaderField:@"UUID"];
-//    (void)[[NSURLConnection alloc] initWithRequest:request delegate:self];
 }
 
 - (void)didReceiveMemoryWarning{
